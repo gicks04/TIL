48edcd51ddc653f0c754a7d6653abee1e38ccb5b
gicks04 <gichanyoon@gmail.com>
2025-08-06 10:58:47 +0900

    파이썬 문제풀이

---
+++ Python/coding-test/boj_2739.py
@@ -0,0 +1,6 @@
+# boj_2739.py
+
+data = int(input())
+
+for i in range(1,10):
+    print(f'{data} * {i} = {data*i}')
---
+++ Python/coding-test/boj_5717.py
@@ -0,0 +1,7 @@
+# boj_5717
+
+M, F = map(int, input().split())
+
+while M != 0:
+    print(f'{M+F}')
+    M, F = map(int, input().split())
